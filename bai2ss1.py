7acd3aa72add7380fe289080570b35f8fa5e9394
vad010107-byte <vad010107@gmail.com>
2026-05-19 11:38:48 +0700

    Add files via upload

---
+++ bai2ss1.py
@@ -0,0 +1,9 @@
+print("--- HỆ THỐNG NHẬP CHỈ SỐ SINH TỒN ---")
+
+name_patient = input("Nhập tên bệnh nhân : ")
+weight = float(input("Nhập cân nặng bệnh nhân : "))
+
+print("--- KIỂM TRA DỮ LIỆU LƯU TRỮ ---")
+print("Bệnh nhân : ", name_patient)
+print("Cân nặng đã nhập : ", weight)
+print("CẢNH BÁO - Kiểu dữ liệu đang lưu là : ", type(weight))
